6bbf5a8e1824f36550c05763118f762cf8dec718
Illia <135041280+IlliaVolobuev@users.noreply.github.com>
2025-12-16 20:50:05 +0100

    deploy2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/historia-xviii-project/' // <-- имя репозитория
+  base: '/Historia-XVIII-Project/'
 })
