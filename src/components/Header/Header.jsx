3a761e596940560d2bd96c145948109f5a3b4b27
Alek <aleksandrvolobuev7676@gmail.com>
2025-12-18 23:05:55 +0200

    пытался сделать роутеры 1 страницы

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { ThemeContext } from "../../context/themeContext"; // <- здесь важно
 import { Link } from "react-router-dom";
 import Icon from "../../assets/images/icon2.svg";
+import ButtonBurger from "../UI/ButtonBurger/ButtonBurger"; // <- импортi
 import "../../App.css";
 import "./Header.css";
-
+import { NavLink } from "react-router-dom";
 export default function Header() {
   const { dark, toggleTheme } = useContext(ThemeContext); // <- исправлено
 
@@ -12,16 +13,20 @@ export default function Header() {
     <header id="header" className="card-glow header-card">
       <img src={Icon} alt="ikonka" className="logo" />
       <nav style={{ display: "flex", gap: "1.5rem" }}>
-        <Link to="/">Strona główna</Link>
-        <Link to="/population">Wzrost ludności</Link>
-        <Link to="/inventions">Wynalazki</Link>
-        <Link to="/transport">Transport i komunikacja</Link>
-        <Link to="/test">Zdać test</Link>
-        <Link to="/game">Gra</Link>
+        <NavLink to="/" end>
+          Strona główna
+        </NavLink>
+        <NavLink to="/population">Wzrost ludności</NavLink>
+        <NavLink to="/inventions">Wynalazki</NavLink>
+        <NavLink to="/transport">Transport i komunikacja</NavLink>
+        <NavLink to="/test">Zdać test</NavLink>
+        <NavLink to="/game">Gra</NavLink>
       </nav>
+
       <button className="theme-toggle" onClick={toggleTheme}>
         {dark ? "Jasny motyw" : "Ciemny motyw"}
       </button>
+      <ButtonBurger />
     </header>
   );
 }
